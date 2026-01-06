77f1933d9ccf9c22f67549d6b034ab7ed8efd0bb
Aditya <adityaverma90055@gmail.com>
2026-01-06 11:34:37 +0530

    Add initial Spring Boot backend project

---
+++ backend/src/main/java/com/auctionbank/auctionbank/EAuctionbankApplication.java
@@ -0,0 +1,13 @@
+package com.auctionbank.auctionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAuctionbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAuctionbankApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/auctionbank/auctionbank/EAuctionbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auctionbank.auctionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAuctionbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
